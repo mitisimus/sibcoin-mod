108b92f47d4a0a0b461c7f6a637c0625356e29de
ivan <ivansib@users.noreply.github.com>
2015-07-15 11:07:54 +0300

    version bump
    Conflicts:
    	src/clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -13,11 +13,11 @@
  * client versioning and copyright year
  */
 
-//! These need to be macros, as clientversion.cpp's and dash*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 0
-#define CLIENT_VERSION_MINOR 12
-#define CLIENT_VERSION_REVISION 3
-#define CLIENT_VERSION_BUILD 2
+// These need to be macros, as version.cpp's and dash-qt.rc's voodoo requires it
+#define CLIENT_VERSION_MAJOR       0
+#define CLIENT_VERSION_MINOR       15
+#define CLIENT_VERSION_REVISION    1
+#define CLIENT_VERSION_BUILD       2
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
