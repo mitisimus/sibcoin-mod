21042d419087a9a13fb7721f82e232530b72c3f4
ivan <ivansib@users.noreply.github.com>
2015-11-30 18:39:57 +0100

    change mac bundle name Conflicts: 	src/qt/macnotificationhandler.mm

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.dash.Dash-Qt";
+        return @"net.sibcoin.Sibcoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
