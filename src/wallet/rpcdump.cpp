7b7fc5391daf4502d4138ff0d61c362102b6b399
ivan <ivansib@users.noreply.github.com>
2015-11-30 18:32:22 +0100

    change error message

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -242,7 +242,7 @@ UniValue importaddress(const JSONRPCRequest& request)
         std::vector<unsigned char> data(ParseHex(request.params[0].get_str()));
         ImportScript(CScript(data.begin(), data.end()), strLabel, fP2SH);
     } else {
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Dash address or script");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Sibcoin address or script");
     }
 
     if (fRescan)
