2d6032c6d621e138e7cbf13b06ff8d61bea04e4d
ivan <ivansib@users.noreply.github.com>
2016-05-28 19:34:46 +0200

    change sha256 names

--- src/bip/sha256.h
+++ src/bip/sha256.h
@@ -37,16 +37,16 @@ typedef struct SHA256Context {
 	uint32_t state[8];
 	uint32_t count[2];
 	unsigned char buf[64];
-} SHA256_CTX;
+} bip_SHA256_CTX;
 
 typedef struct HMAC_SHA256Context {
-	SHA256_CTX ictx;
-	SHA256_CTX octx;
+	bip_SHA256_CTX ictx;
+	bip_SHA256_CTX octx;
 } HMAC_SHA256_CTX;
 
-void	SHA256_Init(SHA256_CTX *);
-void	SHA256_Update(SHA256_CTX *, const void *, size_t);
-void	SHA256_Final(unsigned char [32], SHA256_CTX *);
+void	bip_SHA256_Init(bip_SHA256_CTX *);
+void	bip_SHA256_Update(bip_SHA256_CTX *, const void *, size_t);
+void	bip_SHA256_Final(unsigned char [32], bip_SHA256_CTX *);
 void	HMAC_SHA256_Init(HMAC_SHA256_CTX *, const void *, size_t);
 void	HMAC_SHA256_Update(HMAC_SHA256_CTX *, const void *, size_t);
 void	HMAC_SHA256_Final(unsigned char [32], HMAC_SHA256_CTX *);
