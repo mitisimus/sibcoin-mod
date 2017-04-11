881d7fbbd103206f1556b6d0151bf1878b043706
ivansib <ivansib@users.noreply.github.com>
2017-04-11 14:02:39 +0200

    fix alert and base58 tests

--- contrib/testgen/gen_base58_test_vectors.py
+++ contrib/testgen/gen_base58_test_vectors.py
@@ -18,11 +18,11 @@ import random
 from binascii import b2a_hex
 
 # key types
-PUBKEY_ADDRESS = 76
-SCRIPT_ADDRESS = 16
-PUBKEY_ADDRESS_TEST = 140
-SCRIPT_ADDRESS_TEST = 19
-PRIVKEY = 204
+PUBKEY_ADDRESS = 63
+SCRIPT_ADDRESS = 40
+PUBKEY_ADDRESS_TEST = 125
+SCRIPT_ADDRESS_TEST = 100
+PRIVKEY = 128
 PRIVKEY_TEST = 239
 
 metadata_keys = ['isPrivkey', 'isTestnet', 'addrType', 'isCompressed']
