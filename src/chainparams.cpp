voyager3m/sibcoin

bump version Conflicts: 	src/chainparams.cpp

src/chainparams.cpp
@@ -172,7 +172,9 @@ public:
             (  38700, uint256("0x0000000922c8ae23533c8aa6a4a22f51fa4cdfba85e8c08f2a019dcf755ec48f"))
             (  70000, uint256("0x00000000013eb4498b627e9b8cc1baf74f77f518be4f32ed27b6455e18f5295a")) 
             (  80000, uint256("0x0000000027d43f7c0323d29365f18c39666b8205a160d8a09f599f92ff259482"))
-            ( 116000, uint256("0x0000000002850289e192660166b708ee7ebb44986c04f43c26a21607d77966cd")),
+            ( 116000, uint256("0x0000000002850289e192660166b708ee7ebb44986c04f43c26a21607d77966cd"))
+            ( 180000, uint256("0x00000000091a61016e9e3effdd3a98e6cfac3cb6e3ddda80f2545900abfb0540"))
+            ( 192200, uint256("0x00000000068309e62ecddc6698872f231ff714ce123a7b2c944aa27b02a0e272")),
                     
             1443347415, // * UNIX timestamp of last checkpoint block
             94147,    // * total number of transactions between genesis and last checkpoint

mitisimus/sibcoin-mod

src/chainparams.cpp
@@ -246,7 +246,10 @@ public:
             (  38700, uint256S("0x0000000922c8ae23533c8aa6a4a22f51fa4cdfba85e8c08f2a019dcf755ec48f"))
             (  70000, uint256S("0x00000000013eb4498b627e9b8cc1baf74f77f518be4f32ed27b6455e18f5295a")) 
             (  80000, uint256S("0x0000000027d43f7c0323d29365f18c39666b8205a160d8a09f599f92ff259482"))
-	    ( 116000, uint256S("0x0000000002850289e192660166b708ee7ebb44986c04f43c26a21607d77966cd"))
+            ( 116000, uint256S("0x0000000002850289e192660166b708ee7ebb44986c04f43c26a21607d77966cd"))
+            ( 180000, uint256S("0x00000000091a61016e9e3effdd3a98e6cfac3cb6e3ddda80f2545900abfb0540"))
+            ( 192200, uint256S("0x00000000068309e62ecddc6698872f231ff714ce123a7b2c944aa27b02a0e272"))
+
         };
 
         chainTxData = ChainTxData{