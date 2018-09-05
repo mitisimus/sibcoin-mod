156b43a683fa1fa406be7a744e008aaba6762056
TriKriSta <Trikrista13@gmail.com>
2018-09-05 13:51:22 +0300

    message about large-work fork detected

--- src/validation.cpp
+++ src/validation.cpp
@@ -1406,6 +1406,7 @@ void CheckForkWarningConditions()
             if(pindexBestForkBase->phashBlock){
                 std::string warning = std::string("'Warning: Large-work fork detected, forking after block ") +
                     pindexBestForkBase->phashBlock->ToString() + std::string("'");
+                LogPrintf("%s: Warning: Large-work fork detected, forking after block %s\n", __func__, pindexBestForkBase->phashBlock->ToString());
                 CAlert::Notify(warning);
             }
         }
