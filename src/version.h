8f4cdafe5b20bd4d852bc8cc7b667e7c621f29e0
ivansib <ivansib@users.noreply.github.com>
2018-09-24 12:43:35 +0200

    update protocol version

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70207;
+static const int PROTOCOL_VERSION = 70210;
 static const int OLD_COLLATERAL_PROTOCOL_VERSION = 70206;
 
 //! initial proto version, to be increased after version/verack negotiation
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70206;
+static const int MIN_PEER_PROTO_VERSION = 70207;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
