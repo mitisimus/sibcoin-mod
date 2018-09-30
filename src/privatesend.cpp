757b5a7c0636ef6032854383b8a4c6fba4c844bf
ivansib <ivansib@users.noreply.github.com>
2018-09-30 17:06:08 +0200

    bring back privatesend nominals again

--- src/privatesend.cpp
+++ src/privatesend.cpp
@@ -246,13 +246,13 @@ void CPrivateSend::InitStandardDenominations()
         10DRK+10000 == (1DRK+1000)*10
     */
     /* Disabled
-    vecStandardDenominations.push_back( (100      * COIN)+100000 );
     */
+    vecStandardDenominations.push_back( (100      * COIN)+100000 );
     vecStandardDenominations.push_back( (10       * COIN)+10000 );
     vecStandardDenominations.push_back( (1        * COIN)+1000 );
     vecStandardDenominations.push_back( (.1       * COIN)+100 );
-    vecStandardDenominations.push_back( (.01      * COIN)+10 );
     /* Disabled till we need them
+    vecStandardDenominations.push_back( (.01      * COIN)+10 );
     vecStandardDenominations.push_back( (.001     * COIN)+1 );
     */
 }
