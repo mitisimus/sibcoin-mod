bcf87155dd610a4dff1014f256a279b4f4c6a899
ivansib <ivansib@users.noreply.github.com>
2018-10-01 16:50:54 +0200

    revert default transaction min fee

--- src/validation.h
+++ src/validation.h
@@ -57,7 +57,7 @@ static const bool DEFAULT_WHITELISTRELAY = true;
 /** Default for DEFAULT_WHITELISTFORCERELAY. */
 static const bool DEFAULT_WHITELISTFORCERELAY = true;
 /** Default for -minrelaytxfee, minimum relay fee for transactions */
-static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 1000;
+static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 10000;
 //! -maxtxfee default
 static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.2 * COIN; // "smallest denom" + X * "denom tails"
 //! Discourage users to set fees higher than this amount (in duffs) per kB
